f5b4dbec7e82b118911ed25004cd31a7009bc67f
Abin M K <abinmkabin@gmail.com>
2024-08-12 01:33:20 +0530

    changed ssl

--- backend/server.js
+++ backend/server.js
@@ -32,12 +32,10 @@ app.use('/static', express.static(path.join(__dirname, 'static')));
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'http://ec2-13-235-33-60.ap-south-1.compute.amazonaws.com:5001', 
+    'https://ec2-13-235-33-60.ap-south-1.compute.amazonaws.com:5001', 
     'http://localhost:5173',
     'https://rankandseats.com:5001',
     'http://localhost:5001',
-    'http://ec2-52-63-164-69.ap-southeast-2.compute.amazonaws.com:5001',
-    'https://ec2-52-63-164-69.ap-southeast-2.compute.amazonaws.com:5001',
     'http://rankseatsbucket.s3-website-ap-southeast-2.amazonaws.com', 
     'https://rankseatsbucket.s3-website-ap-southeast-2.amazonaws.com'
   ],
