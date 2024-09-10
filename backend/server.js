8da69918541a3bfe6d13a1fe10d6a4a57c6959ac
Abin M K <abinmkabin@gmail.com>
2024-09-10 20:08:25 +0530

    fixed issue with upload

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,9 @@ const profileRoutes = require('./routes/profileRoutes');
 const app = express();
 const port = process.env.PORT || 5001;
 
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 // Serve the React build directory
 app.use(express.static(path.join(__dirname, 'build')));
 
@@ -371,3 +374,5 @@ app.use((err, req, res, next) => {
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+
