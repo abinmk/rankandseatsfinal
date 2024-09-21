3dc814de7d0dabb5aec6986ddb359e1a6aa0623f
Abin M K <abinmkabin@gmail.com>
2024-09-21 13:59:36 +0530

    added gtag

--- frontend/src/pages/App.jsx
+++ frontend/src/pages/App.jsx
@@ -8,6 +8,7 @@ import Pageheader from "../components/pageheader/pageheader";
 import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../components/common/footer/footer";
 import Tabtotop from "../components/common/tab-to-tap/tabtotap";
+import { Helmet } from "react-helmet-async";
 
 function App() {
   const [lateLoad, setLateLoad] = useState(false);
@@ -24,6 +25,18 @@ function App() {
 
   return (
     <Fragment>
+        {/* <Helmet>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-FVHKKYYQ2E"></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-FVHKKYYQ2E');
+          `}
+        </script>
+      </Helmet> */}
       <Provider store={store}>
         <div style={{ display: `${lateLoad ? "block" : "none"}` }}>
           <Switcher />
